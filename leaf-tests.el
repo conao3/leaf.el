5575b3e17af850886da78e505c8f3202b41617e7
conao3 <conao3@gmail.com>
2019-05-08 04:28:00 +0900

    add :hook testcases

--- leaf-tests.el
+++ leaf-tests.el
@@ -988,5 +988,50 @@ Example
        (add-to-list 'magic-fallback-mode-alist '("html?" web-html-mode))
        (add-to-list 'magic-fallback-mode-alist '("js" web-mode))))))
 
+(cort-deftest-with-macroexpand leaf/hook
+  '(((leaf ace-jump-mode
+       :hook cc-mode-hook)
+     (progn
+       (autoload (function ace-jump-mode) "ace-jump-mode" nil t)
+       (add-hook 'cc-mode-hook (function ace-jump-mode))))
+
+    ((leaf ace-jump-mode
+       :hook cc-mode-hook prog-mode-hook)
+     (progn
+       (autoload (function ace-jump-mode) "ace-jump-mode" nil t)
+       (add-hook 'cc-mode-hook (function ace-jump-mode))
+       (add-hook 'prog-mode-hook (function ace-jump-mode))))
+
+    ((leaf ace-jump-mode
+       :hook (cc-mode-hook (prog-mode-hook)))
+     (progn
+       (autoload (function ace-jump-mode) "ace-jump-mode" nil t)
+       (add-hook 'cc-mode-hook (function ace-jump-mode))
+       (add-hook 'prog-mode-hook (function ace-jump-mode))))
+
+    ((leaf ace-jump-mode
+       :hook cc-mode-hook (prog-mode-hook . my-ace-jump-mode))
+     (progn
+       (autoload (function ace-jump-mode) "ace-jump-mode" nil t)
+       (autoload (function my-ace-jump-mode) "ace-jump-mode" nil t)
+       (add-hook 'cc-mode-hook (function ace-jump-mode))
+       (add-hook 'prog-mode-hook (function my-ace-jump-mode))))
+
+    ((leaf ace-jump-mode
+       :hook ((cc-mode-hook prog-mode-hook) . my-ace-jump-mode))
+     (progn
+       (autoload (function my-ace-jump-mode) "ace-jump-mode" nil t)
+       (add-hook 'cc-mode-hook (function my-ace-jump-mode))
+       (add-hook 'prog-mode-hook (function my-ace-jump-mode))))
+
+    ((leaf ace-jump-mode
+       :hook ((cc-mode-hook prog-mode-hook . ace-jump-mode) isearch-mode . my-ace-jump-mode))
+     (progn
+       (autoload (function ace-jump-mode) "ace-jump-mode" nil t)
+       (autoload (function my-ace-jump-mode) "ace-jump-mode" nil t)
+       (add-hook 'cc-mode-hook (function ace-jump-mode))
+       (add-hook 'prog-mode-hook (function ace-jump-mode))
+       (add-hook 'isearch-mode (function my-ace-jump-mode))))))
+
 (provide 'leaf-tests)
 ;;; leaf-tests.el ends here
