d7e6b3ddd6fc91a1626ab152d4c09bbfbbe6e578
conao3 <conao3@gmail.com>
2019-05-26 09:51:09 +0900

    add description in Commentary section

--- leaf.el
+++ leaf.el
@@ -25,9 +25,23 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary:pp
+;;; Commentary:
 
-;; simpify init.el
+;; Provides macros that allow you to declaratively configure settings typical
+;; of an Elisp package with various keywords.
+;;
+;; By separating the settings of a package and combining many 'leaves' of a
+;; package's settings, you could make a 'Yggdrasill' on top of your Emacs.
+;;
+;; A leaf can consist of multiple packages, in which case you can disable all
+;; dependent child packages by disabling one parent's package.
+;;
+;; It also has a key management system and package management uses the
+;; package.el.  With minimal external dependencies and careful implementation,
+;; this package is guaranteed to be fully functional from Emacs-22, now, and
+;; in future Emacs.
+;;
+;; More information is [[https://github.com/conao3/leaf.el][here]]
 
 ;;; Code:
 
