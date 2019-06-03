636f030c49f60c089b4f71807b79578a05e724c5
conao3 <conao3@gmail.com>
2019-06-04 03:02:37 +0900

    tagged v3.1.7

--- leaf.el
+++ leaf.el
@@ -5,7 +5,7 @@
 ;; Author: Naoya Yamashita <conao3@gmail.com>
 ;; Maintainer: Naoya Yamashita <conao3@gmail.com>
 ;; Keywords: lisp settings
-;; Version: 3.1.6
+;; Version: 3.1.7
 ;; URL: https://github.com/conao3/leaf.el
 ;; Package-Requires: ((emacs "24.4"))
 
