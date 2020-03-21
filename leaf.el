83f44f701ee3211f756a1266bd2b7b3d9b5c94e3
conao3 <conao3@gmail.com>
2020-03-21 14:29:44 +0900

    fill paragraph

--- leaf.el
+++ leaf.el
@@ -25,19 +25,21 @@
 
 ;;; Commentary:
 
-;; Provides macros that allow you to declaratively configure settings typical
-;; of an Elisp package with various keywords.
+;; Provides macros that allow you to declaratively configure
+;; settings typical of an Elisp package with various keywords.
 
-;; By separating the settings of a package and combining many 'leaves' of a
-;; package's settings, you could make a 'Yggdrasill' on your Emacs.
+;; By separating the settings of a package and combining many
+;; 'leaves' of a package's settings, you could make a 'Yggdrasill'
+;; on your Emacs.
 
-;; A leaf can consist of multiple packages, in which case you can disable all
-;; dependent child packages by disabling one parent's package.
+;; A leaf can consist of multiple packages, in which case you can
+;; disable all dependent child packages by disabling one parent's
+;; package.
 
-;; It also has a key management system and package management uses the
-;; package.el.  With minimal external dependencies and careful implementation,
-;; this package is guaranteed to be fully functional from Emacs-24.4, now, and
-;; in future Emacs.
+;; It also has a key management system and package management
+;; uses the package.el.  With minimal external dependencies and
+;; careful implementation, this package is guaranteed to be fully
+;; functional from Emacs-24.4, now, and in future Emacs.
 
 ;; More information is [[https://github.com/conao3/leaf.el][here]]
 
