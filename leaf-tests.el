841847361405bb01bb116468a6863191490ece57
Naoya Yamashita <conao3@gmail.com>
2021-11-12 00:02:21 +0900

    transform nil to ()

--- leaf-tests.el
+++ leaf-tests.el
@@ -2454,7 +2454,7 @@ Example:
        (let* ((old (lookup-key global-map (kbd "M-s O")))
               (value `(global-map "M-s O" *lambda-function* ,(and old (not (numberp old)) old) nil)))
          (leaf-safe-push value leaf-key-bindlist)
-         (define-key global-map (kbd "M-s O") (lambda nil "color-moccur" (interactive) (color-moccur))))))))
+         (define-key global-map (kbd "M-s O") (lambda () "color-moccur" (interactive) (color-moccur))))))))
 
 (when (version< "24.0" emacs-version)
   (cort-deftest-with-macroexpand leaf/leaf-key-bind-keymap
